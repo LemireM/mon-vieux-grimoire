df171b2a23f20edce7698e83ce7c265ee485b712
Mael Lemire <mael.lemire@icloud.com>
2024-11-26 17:31:35 +0100

    verify token on going

--- backend/middlewares/authentification.js
+++ backend/middlewares/authentification.js
@@ -1,7 +1,16 @@
+const jwt = require('jsonwebtoken');
+const { JWT_SECRET } = process.env;
+
 const authMiddleware = function (req, res, next) {
     //TODO: Verficiation de token avec JWT.verify
-    res.status(401).end()    
+    const token = req.headers.authorization
+    if (!token)
+      return res.status(401).end()
+    jwt.verify(token, JWT_SECRET, (err, decoded) => {
+      if (err)
+        return res.status().end()
+      req
     next()
   }
-
+}
 module.exports = authMiddleware
